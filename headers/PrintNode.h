8928bd1712e921e24f079d28b01d8710c2bb82af
ilicandjela <mladostmidji@gmail.com>
2020-12-08 22:14:52 +0100

    feat:Implementirana logika za PrintNode
    *Izmenjen izgled print noda
    *Print node sada radi u skladu sa parserom
    *Omogucen print iz tekstualnog fajla, kao i ono sto je manualno uneto

--- headers/PrintNode.h
+++ headers/PrintNode.h
@@ -5,11 +5,19 @@
 #include <QTextEdit>
 #include <QFont>
 #include <QFormLayout>
-
+#include <QTextEdit>
+#include <QAbstractButton>
+#include <QRadioButton>
+#include <QButtonGroup>
 #include "./headers/Node.h"
 
 class PrintNode : public Node
 {
+private:
+    bool manualInput;
+    bool fileInput;
+    QTextEdit* input;
+
 public:
     PrintNode();
 
--- src/PrintNode.cpp
+++ src/PrintNode.cpp
@@ -9,30 +9,100 @@
 
 PrintNode::PrintNode() : Node("print", 1, 1)
 {
-    setMinimumSize(100,100);
+    setMinimumSize(200,300);
     setMaximumWidth(200);
     setStyleSheet ("background-color: rgba(105, 50, 129, 1);"
                    "border: 1px solid rgba(194, 145, 211, 1);");
-    QTextEdit* edit = new QTextEdit();
-
-    const QString* font = new QString("Courier New");
-    const QString* placeHolder = new QString("Output...");
-    edit->setFontFamily(*font);
-    edit->setPlaceholderText(*placeHolder);
-    edit->setReadOnly(true);
-    //edit->setText("Danas je lep dan");
-
     QGridLayout* layout = static_cast<QGridLayout*>(this->layout());
-    layout->addWidget(edit,2,0);
-}
 
-/*F-ja koja vraca code za dati cvor
-*TODO
-*/
+    QRadioButton* fileInputRadioButton = new QRadioButton("Input from file",this);
+    QRadioButton *manualInputRadioButton = new QRadioButton("Manula input",this);
+
+    QButtonGroup *choice = new QButtonGroup(this);
+    choice->addButton(fileInputRadioButton, 1);
+    choice->addButton(manualInputRadioButton, 2 );
+
+    connect(choice, QOverload<QAbstractButton *>::of(&QButtonGroup::buttonClicked),
+            [=](){
+
+        if(this->fileInput || this->manualInput){
+            if(this->input != nullptr){
+                this->input->deleteLater();
+                this->input = nullptr;
+            }
+            this->fileInput = false;
+            this->manualInput = false;
+        }
+
+        int choosen = choice->checkedId();
+        if(choosen == 1){
+            QTextEdit* fileEdit = new QTextEdit();
+            const QString* font = new QString("Courier New");
+            const QString* placeHolder = new QString("Enter file location here...");
+            fileEdit->setFontFamily(*font);
+            fileEdit->setPlaceholderText(*placeHolder);
+            //QString input = lineEdit->text();
+            //this->fileName = new QString(input);
+            Node* parent = static_cast<Node*>(choice->parent());
+            QGridLayout* layout = static_cast<QGridLayout*>(parent->layout());
+            layout->addWidget(fileEdit,4,0);
+            this->fileInput = true;
+            this->input = fileEdit;
+        }
+    });
+    connect(choice, QOverload<QAbstractButton *>::of(&QButtonGroup::buttonClicked),
+            [=](){
+
+        if(this->manualInput){
+            if(this->input != nullptr){
+                this->input->deleteLater();
+                this->input = nullptr;
+                this->manualInput = false;
+            }
+            if(this->fileInput){
+                this->input->deleteLater();
+                this->input = nullptr;
+                this->fileInput = false;
+            }
+        }
+
+        int choosen = choice->checkedId();
+        if(choosen == 2){
+            QTextEdit* edit = new QTextEdit();
+            const QString* font = new QString("Courier New");
+            const QString* placeHolder = new QString("Enter text here...");
+            edit->setFontFamily(*font);
+            edit->setPlaceholderText(*placeHolder);
+            Node* parent = static_cast<Node*>(choice->parent());
+            QGridLayout* layout = static_cast<QGridLayout*>(parent->layout());
+            layout->addWidget(edit,4,0);
+            this->manualInput = true;
+            this->input = edit;
+        }
+    });
+    layout->addWidget(fileInputRadioButton,2,0);
+    layout->addWidget(manualInputRadioButton,3,0);
+}
 QString PrintNode::getCodeForNode()
 {
-    QString text = "#include<isotream>\n#include<string>\n\nint main(){\n";
-    qDebug()<<"Print";
+    QString text;
+        text.push_back("std::cout<<\"");
+        if(this->manualInput){
+            QString tmpText = this->input->toPlainText();
+            text.push_back(tmpText);
+        }
+        if(this->fileInput){
+            QString fileName = this->input->toPlainText();
+            std::fstream fdata(fileName.toUtf8().constData());
 
-    return text;
+            char c;
+            while(fdata.get(c)){
+                text.push_back(c);
+            }
+
+        }
+        text = text.simplified();
+        text.push_back("\"<<std::endl;\n");
+        return text;
+
 }
