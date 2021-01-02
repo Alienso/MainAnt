3046db95a1df80a34f5b2b8f205eb201da41b266
Mirko <ilicmirko07@gmail.com>
2021-01-02 12:52:31 +0100

    feat:Dodato da vectro cvor moze da se inicializuje ili samo deklarise po izboru

--- headers/nodesHeaders/VectorNode.h
+++ headers/nodesHeaders/VectorNode.h
@@ -5,20 +5,41 @@
 #include <QComboBox>
 #include <QComboBox>
 #include <QGridLayout>
+#include <QButtonGroup>
+#include <QRadioButton>
+#include <QPushButton>
 
 #include "./headers/Node.h"
 
 class VectorNode : public Node
-{       
+{
+      Q_OBJECT
 public:
     VectorNode();
 
     QLineEdit* vectorName;
     QLineEdit* capacity;
     QComboBox* varTypes;
+    QVector<QString> initializedVars;
+
+    QLineEdit* in;
+    QLineEdit* lastFive;
+    QPushButton* addVar;
+    QPushButton* removeVar;
+    QString lasFiveText = "empty vector";
+    QButtonGroup *choice;
+    QRadioButton* initialize;
+    QRadioButton* notInitialize;
+
+    int initialSize = 220;
 
     QString getCodeForNode() override;
     QString getVarName() const override;
+    void genererateLastFiveText();
+
+public slots:
+    void addVariable();
+    void removeVariable();
 };
 
 #endif // VECTORNODE_H
--- src/nodes/VectorNode.cpp
+++ src/nodes/VectorNode.cpp
@@ -2,7 +2,7 @@
 
 VectorNode::VectorNode() : Node("vector", 1, 1)
 {
-    setMinimumSize(180,150);
+    setMinimumSize(200,this->initialSize);
     setStyleSheet ("background-color: rgba(38, 55, 114, 1);"
                    "border: 1px solid rgba(255, 150, 132, 1);"
                    "border-radius:5px;");
@@ -19,6 +19,13 @@ VectorNode::VectorNode() : Node("vector", 1, 1)
     const QString* placeHolderVarName = new QString("Set vector size...");
     this->capacity->setPlaceholderText(*placeHolderVarName);
 
+    this->in = new QLineEdit();
+    const QString* placeHolderValueIn = new QString("Enter var value...");
+    this->in->setPlaceholderText(*placeHolderValueIn);
+
+    this->lastFive = new QLineEdit();
+    this->lastFive->setReadOnly(true);
+
     this->varTypes = new QComboBox();
     this->varTypes->addItem("Int");
     this->varTypes->addItem("Float");
@@ -27,16 +34,70 @@ VectorNode::VectorNode() : Node("vector", 1, 1)
     this->varTypes->addItem("Char");
     this->varTypes->addItem("String");
 
+    this->initialize = new QRadioButton("Initialize",this);
+    this->notInitialize = new QRadioButton("Declare", this);
+
+    this->notInitialize->setChecked(true);
+
+    this->choice = new QButtonGroup(this);
+    this->choice->addButton(this->notInitialize, 1);
+    this->choice->addButton(this->initialize, 2);
+
+    this->addVar = new QPushButton("+");
+    this->removeVar = new QPushButton("-");
+
+    connect(this->choice, QOverload<QAbstractButton *>::of(&QButtonGroup::buttonClicked),
+            [=](){
+
+        int choosen = this->choice->checkedId();
+        if(choosen == 1){
+            this->capacity->show();
+            this->in->hide();
+            this->lastFive->hide();
+            this->addVar->hide();
+            this->removeVar->hide();
+
+            setMinimumSize(200, this->initialSize);
+        }else if(choosen == 2){
+            this->capacity->hide();
+            this->in->show();
+            this->lastFive->show();
+            this->addVar->show();
+            this->removeVar->show();
+
+            setMinimumSize(200, this->initialSize + 40);
+        }
+
+    });
+
     layout->addWidget(this->varTypes, 3, 0);
     layout->addWidget(this->vectorName, 4, 0);
     layout->addWidget(this->capacity, 5, 0);
+    layout->addWidget(this->notInitialize, 6, 0);
+    layout->addWidget(this->initialize, 7, 0);
+    layout->addWidget(this->in, 8, 0);
+    layout->addWidget(this->lastFive, 9, 0);
+    layout->addWidget(this->addVar, 8, 1);
+    layout->addWidget(this->removeVar, 9, 1);
+
+
+    connect(this->addVar, SIGNAL(clicked()), this, SLOT(addVariable(void)));
+    connect(this->removeVar, SIGNAL(clicked()), this, SLOT(removeVariable(void)));
+
+    this->addVar->hide();
+    this->lastFive->hide();
+    this->in->hide();
+    this->removeVar->hide();
+    this->lastFive->setText(this->lasFiveText);
+
 }
 
 QString VectorNode::getCodeForNode()
 {
     QString text = "vector<";
 
-    if(this->varTypes->currentText() == QString::fromStdString("String")){
+    if(this->varTypes->currentText() == QString::fromStdString("String"))
+    {
         text+= QString::fromStdString("std::string>");
     }else{
         text+= varTypes->currentText().toLower();
@@ -44,12 +105,41 @@ QString VectorNode::getCodeForNode()
     }
 
     text+= this->vectorName->text();
-    if(this->capacity->text().length() != 0){
-        text+=QString::fromStdString("(");
-        text+=this->capacity->text();
-        text+=QString::fromStdString(")");
+    if(this->choice->checkedId() == 1)
+    {
+        if(this->capacity->text().length() != 0){
+            text+=QString::fromStdString("(");
+            text+=this->capacity->text();
+            text+=QString::fromStdString(")");
+        }
+    }else
+    {
+        if(this->varTypes->currentText() == QString::fromStdString("String"))
+        {
+            text+= QString::fromStdString("{");
+            for(int i = 0; i<this->initializedVars.length(); i++){
+                text+= QString::fromStdString("\"");
+                text+= this->initializedVars[i];
+                text+= QString::fromStdString("\"");
+                if(i < this->initializedVars.length()-1)
+                {
+                    text+= QString::fromStdString(", ");
+                }
+            }
+            text+= QString::fromStdString("}");
+        }else
+        {
+            text+= QString::fromStdString("{");
+            for(int i = 0; i<this->initializedVars.length(); i++){
+                text+= this->initializedVars[i];
+                if(i < this->initializedVars.length()-1)
+                {
+                    text+= QString::fromStdString(", ");
+                }
+            }
+            text+= QString::fromStdString("}");
+        }
     }
-
     text+= QString::fromStdString(";\n");
 
     return text;
@@ -60,4 +150,52 @@ QString VectorNode::getVarName() const
 {
     QString text = this->vectorName->text();
     return text;
+}
+
+void VectorNode::addVariable()
+{
+    qDebug()<<"Dodajem argument";
+    if(this->in->text() != QString::fromStdString("")){
+
+        if(this->varTypes->currentText() != QString::fromStdString("String"))
+        {
+            this->initializedVars.push_back(this->in->text().trimmed());
+        }else
+        {
+            this->initializedVars.push_back(this->in->text());
+        }
+        this->in->setText("");
+        this->genererateLastFiveText();
+    }
+}
+
+void VectorNode::removeVariable()
+{
+    if(this->initializedVars.empty() != true){
+        this->initializedVars.pop_back();
+    }
+    this->genererateLastFiveText();
+}
+
+
+void VectorNode::genererateLastFiveText()
+{
+    this->lasFiveText = "";
+    if(this->initializedVars.length() > 5){
+        for(int i= this->initializedVars.length() - 5; i<this->initializedVars.length(); i++){
+            lasFiveText+=this->initializedVars[i];
+            lasFiveText+=QString::fromStdString(" ");
+        }
+
+    }else{
+        for(int i= 0; i<this->initializedVars.length(); i++){
+            lasFiveText+=this->initializedVars[i];
+            lasFiveText+=QString::fromStdString(" ");
+        }
+    }
+    if(this->initializedVars.empty())
+    {
+        lasFiveText+=QString::fromStdString("empty vector");
+    }
+    this->lastFive->setText(this->lasFiveText);
 }
